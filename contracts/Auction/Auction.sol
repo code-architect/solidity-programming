1025712fa2305718c0cddea84d8bc9bc377f4561
code-architect <codearchitectdeveloper@gmail.com>
2024-02-22 22:40:18 +0530

    using openzeppelin library for math, but also have written the code for min value,

--- contracts/Auction/Auction.sol
+++ contracts/Auction/Auction.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 contract Auction 
 {
     address payable public owner;
@@ -25,5 +27,50 @@ contract Auction
         endBlock = startBlock + 40320;  // the auction will run for a week and every 15 second a nw blockis being minined! dot the math
         ipfsHash = "";
         bidIncrement = 100;
+    }
+
+    //===================================================== Modifier and helpers Starts ==================================================================
+    modifier notOwner()
+    {
+        require(msg.sender != owner, "You are not allowed to increase the bid");
+        _;
+    }
+
+    modifier afterStart()
+    {
+        require(block.number >= startBlock);
+        _;
+    }
+
+    modifier beforeEnd()
+    {
+        require(block.number <= endBlock);
+        _;
+    }
+
+    function min(uint a, uint b) pure internal returns (uint)
+    {
+        if(a <= b)
+        {
+            return a;
+        }else {
+            return b;
+        }
+    }
+
+    //====================================================== Modifier Ends ===================================================================
+    function placeBid() public payable notOwner afterStart beforeEnd 
+    {
+        require(auctionState == State.Running);
+        require(msg.value >= 100);
+
+        uint currentBid = bids[msg.sender] + msg.value;
+        require(currentBid > highestBindingnBid);
+
+        bids[msg.sender] = currentBid;
+        if(currentBid <= bids[highetBidder])
+        {
+            highestBindingnBid = Math.min(currentBid + bidIncrement, bids[highetBidder]);
+        }
     }
 }
