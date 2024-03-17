352de10e6bd5691f5b05b3561d82b3c26c9da143
tas-indranil <codearchitectdeveloper@gmail.com>
2024-03-17 07:35:40 +0530

    bug fixed, make sure not to use the latest version because the plugins do not support

---
+++ hardhat-testing-1/readme.md
@@ -0,0 +1,11 @@
+# Notes
+
+## Installation
+- `npm install --save-dev hardhat`
+- `npx hardhat init`
+-  i have chosen the basic one
+- `npm i @nomicfoundation/hardhat-toolbox`
+
+
+## Commands
+`npx hardhat run --network localhost scripts\deploy.js`
--- hardhat-testing-1/scripts/deploy.js
+++ hardhat-testing-1/scripts/deploy.js
@@ -4,16 +4,16 @@ const hre = require("hardhat");
 
 async function main() 
 {
-    const currentTimestampInSeconds = Math.floor(Date.now() / 1000)
+    const currentTimestampInSeconds = Math.random(Date.now() / 1000);
     const ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60;
     const unlockedTime = currentTimestampInSeconds + ONE_YEAR_IN_SECONDS;
-    const lockedAmount = hre.ethers.utils.parseEther("1");
+    const lockedAmount = hre.ethers.parseEther("1");
 
     const MyTest = await hre.ethers.getContractFactory("MyTest");
-    const myTest = await MyTest.deploy(unlockedTime, {value: lockedAmount});
+    const myTest = await MyTest.deploy(unlockedTime);
 
-    await myTest.deployed();
-    console.log(myTest.address);
+    await myTest.waitForDeployment();
+    console.log(myTest.target);
 }
 
 main().catch((error) => {
