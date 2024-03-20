2de314a42efa5260227066578fcf6ac2d8b3a0d5
tas-indranil <codearchitectdeveloper@gmail.com>
2024-03-21 00:52:28 +0530

    created base of design pattern of Access Control Lists (ACLs)

---
+++ solidity-design-pattern/contracts/ACL.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract ACL
+{
+    enum Role { None, User, Admin }
+    mapping(address => Role) public roles;
+
+    event RoleGranted(address indexed addr, Role role);
+    event RoleRevoked(address indexed addr, Role role);
+
+    modifier onlyRole(Role _role)
+    {
+        require(hasRole(msg.sender, _role), "No permission to perform this action");
+        _;
+    }
+
+    // This function allows anyone to check if a given address (_addr) has a specific role (_role). It returns true if the address has the specified role, and false otherwise.
+    function hasRole(address _addr, Role _role) public view returns(bool)
+    {
+        return roles[_addr] == _role;
+    }
+
+    function grantRole(address _addr, Role _role) public onlyRole(Role.Admin)
+    {
+        roles[_addr] = _role;
+        emit RoleGranted(_addr, _role);
+    }
+
+    function revokeRole(address _addr, Role _role) public onlyRole(Role.Admin)
+    {
+        roles[_addr] = Role.None;
+        emit RoleRevoked(_addr, _role);
+    }
+}
+
+contract MyContract is ACL {
+    
+}
