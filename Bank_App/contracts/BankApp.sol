dce223858e37112731f5fa4401ec3b8d6d8eafdf
tas-indranil <codearchitectdeveloper@gmail.com>
2024-03-26 23:30:04 +0530

    created bank app methods

--- Bank_App/contracts/BankApp.sol
+++ Bank_App/contracts/BankApp.sol
@@ -3,7 +3,45 @@ pragma solidity ^0.8.0;
 
 contract BankApp
 {
+    // owner
+    address public owner;
+
+    constructor() 
+    {
+        owner = msg.sender;
+    }
+
+    // user balance
+    mapping(address => uint) private userBalance;
+
     // deposit
+    function deposit() public payable
+    {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        userBalance[msg.sender] += msg.value;
+    }
+
     // withdraw
+    function withdrawl(uint amount) public 
+    {
+        require(amount <= userBalance[msg.sender], "You do not have sufficient  balance");        
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Amount Transfer failed");
+        userBalance[msg.sender] -= amount;
+    }
+
     // balance
+    function viewBalance() public view returns(uint)
+    {
+        return userBalance[msg.sender];
+    }
+
+    // change account balance
+    function creditOwner(address account, uint amount) public 
+    {
+        require(msg.sender == owner, "You do not have the permissions. Only te owner can change the balance");
+        require(amount > 0, "Deposit amount must be greater than 0");
+        userBalance[account] += amount;
+    }
 }
+
