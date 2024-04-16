549c652df630a4efb1c18c6337cb499a75378063
tas-indranil <codearchitectdeveloper@gmail.com>
2024-04-16 14:58:52 +0530

    checking if a given address is a contract or not using inline assembly

---
+++ solidity_questions/interesting_questions/1_if_contract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+// Write a Solidity function to check if a given address is a contract or not.
+
+contract CheckContract 
+{
+    function at(address addr) public view returns (bytes memory code) {
+        assembly {
+            // retrieve the size of the code, this needs assembly
+            let size := extcodesize(addr)
+            // allocate output byte array - this could also be done without assembly
+            // by using code = new bytes(size)
+            code := mload(0x40)
+            // new "memory end" including padding
+            mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            // store length in memory
+            mstore(code, size)
+            // actually retrieve the code, this needs assembly
+            extcodecopy(addr, add(code, 0x20), 0, size)
+        }
+    }  
+}
